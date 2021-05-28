71e81d5dd3d474bbc78cdd4967eba92c85a56263
pedropadilha02 <pedropadilha02@gmail.com>
2021-05-28 11:23:00 -0300

    Adicionado mensagens de erro em casos de pesquisa outrange

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import React, {useState} from 'react';
 import './App.css';
-import {InputGroup, Input, InputGroupAddon, Button, FormGroup, Label} from 'reactstrap'
+import {InputGroup, Input, InputGroupAddon, Button, FormGroup, Label} from 'reactstrap';
 import 'react-toastify/dist/ReactToastify.css';
-import {toast, ToastContainer, toastContainer, tost} from 'react-toastify'
+import {toast, ToastContainer, toastContainer, tost} from 'react-toastify';
+import axios from 'axios';
+
 function App() {
   
   // States
-  const [maxResults, setMaxResults] = useState(10)
-  const [startIndex, setStartIndex] = useState(1)
+  const [maxResults, setMaxResults] = useState(10);
+  const [startIndex, setStartIndex] = useState(1);
   const [query, setQuery] = useState('');
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const axios = require('axios');
   
   //Handle Search
   const handleSubmit = () => {
     setLoading(true);
-    if(maxResults > 40 || maxResults < 1) {
-      toast.error('max results must be between 1 and 40')
+    if (maxResults > 40 || maxResults < 1) {
+      toast.error('max results must be between 1 and 40');
+    } else {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
+        ).then(res => {
+        if(startIndex >= res.data.totalItems || startIndex < 1) {
+          toast.error(`max reults must be between 1 and ${res.data.totalItems}`)
+        }console.log(res.data)
+      }).catch(err => {
+        console.log(err)
+      });
     }
   };  
   // Main Show Case
