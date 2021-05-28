0512f34dad6d7ea5e1476d6c9bdb7497bc272c1b
pedropadilha02 <pedropadilha02@gmail.com>
2021-05-28 12:13:13 -0300

    Criado cartão de livro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import './App.css';
-import {InputGroup, Input, InputGroupAddon, Button, FormGroup, Label} from 'reactstrap';
+import {InputGroup, Input, InputGroupAddon, Button, FormGroup, Label, Spinner} from 'reactstrap';
 import 'react-toastify/dist/ReactToastify.css';
 import {toast, ToastContainer, toastContainer, tost} from 'react-toastify';
 import axios from 'axios';
+import BookCard from './BookCard.jsx'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
   const axios = require('axios');
+  const [cards, setCards] = useState([])
   
   //Handle Search
   const handleSubmit = () => {
@@ -23,12 +25,23 @@ function App() {
       axios
         .get(
           `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
-        ).then(res => {
-        if(startIndex >= res.data.totalItems || startIndex < 1) {
-          toast.error(`max reults must be between 1 and ${res.data.totalItems}`)
-        }console.log(res.data)
-      }).catch(err => {
-        console.log(err)
+        )
+        .then(res => {
+          if (startIndex >= res.data.totalItems || startIndex < 1) {
+            toast.error(
+              `max reults must be between 1 and ${res.data.totalItems}`
+            );
+          } else {
+            if (res.data.items.length > 0) {
+              setCards(res.data.items)
+              setLoading(false) 
+            }
+          }
+          console.log(cards)
+      })
+      .catch(err => {
+        setLoading(true)
+        toast.error(`${err.response.data.error.message}`)
       });
     }
   };  
@@ -80,10 +93,42 @@ function App() {
       </div>
     );
   };
-  return <div>
+
+  const handleCards = () => {
+    console.log(cards)
+    const items = cards.map ((item, i) => {
+      let thumbnail = '';
+      if(item.volumeinfo.imageLinks.thumbnail){
+        thumbnail = item.volumeinfo.imageLinks.thumbnail;
+      }
+
+      return(
+        <div className="col-lg-4" key={ item.id } >
+          <BookCard thumbnail={ thumbnail}/>
+        </div>
+      )
+    })
+    if(loading) {
+      return(
+        <div className='d-flex justify-content-center mt-3' >
+          <Spinner style={{width: '3rem', height: '3rem' }}/>
+        </div>
+      ) 
+      } else {
+        return(
+          <div className='container my-5'>
+            <div className="row">{ items}</div>
+          </div>
+        );
+    }
+  };
+  return (
+    <div className='w-100 h-100'>
     {mainHeader()}
+    {handleCards()}
     <ToastContainer />
-    </div>;
+    </div>
+  );
 }
 
 export default App;
---
+++ src/BookCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Card, CardTitle, CardImg, CardBody, Button, Modal, } from 'reactstrap'
+const BookCard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BookCard
